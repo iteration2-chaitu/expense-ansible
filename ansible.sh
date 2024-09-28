65a685acf3e281e0650b17fac623105331829d37
chaitu <chaitus.ch09@gmail.com>
2024-09-28 07:45:25 -0700

    try doing this

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,6 @@
 component=$1
-ansible-playbook -i 172.31.29.173,  -e ansible_user=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=$component -e env=dev
+ansible-playbook -i Hosts -e ansible_user=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=$component -e env=dev
+
+
+,
 # component =frontend
