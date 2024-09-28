80cbfe64028592b22593600609171a705468a3c6
chaitu <chaitus.ch09@gmail.com>
2024-09-28 10:25:27 -0700

    creating the Hosts file for pipeline

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 component=$1
-ansible-playbook -i frontend-dev-yml.chaitus.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=$component -e env=dev
+ansible-playbook -i frontend-dev-yml.chaitus.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=dev
 
 # component =frontend
