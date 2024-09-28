41ee10ad5bf38993ca6418c863a82a2c13122aab
chaitu <chaitus.ch09@gmail.com>
2024-09-27 22:17:49 -0700

    GOCD environment for frontend

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
-ansible-playbook -i frontend-dev-yml.chaitus.shop  -e ansible_user=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=$component -e env=dev
+ansible-playbook -i frontend-dev-yml.chaitus.shop,  -e ansible_user=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=$component -e env=dev
 # component =frontend
