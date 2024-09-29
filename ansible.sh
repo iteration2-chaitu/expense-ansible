c127f76401f62eb6ae2e6abf7747f480a97c1cf4
chaitu <chaitus.ch09@gmail.com>
2024-09-28 17:13:42 -0700

    creating the 4 stages in the front end pipeline(DEV,QA,UAT,prod)

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 component=$1
-ansible-playbook -i frontend-dev-yml.chaitus.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=dev
+ansible-playbook -i $component-$env-yml.chaitus.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=$env
+#env=dev
 
 # component =frontend
