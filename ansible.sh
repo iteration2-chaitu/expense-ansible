1eec23c1cf27fb575cd2e2c0afcdb9ac5309c03e
chaitu <chaitus.ch09@gmail.com>
2024-10-07 21:25:49 -0700

    checking with terraform

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 component=$1
-ansible-playbook -i $component-$env.chaitus.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=$env
+ansible-playbook -i localhost, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=$env
+#$component-$env.chaitus.shop
 #$component-$env-yml.chaitus.shop
 #env=dev
 
