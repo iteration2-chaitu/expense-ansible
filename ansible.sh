f08d05321a72162b18210daf108bfe1419705f22
chaitu <chaitus.ch09@gmail.com>
2024-10-08 10:42:11 -0700

    to run ansible via gocd pielines

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 component=$1
-ansible-playbook -i localhost, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=$env
+ansible-playbook -i $component-$env-yml.chaitus.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=$env
 
 #$component-$env-yml.chaitus.shop ....FOR ANSIBLE
+#localhost
 #env=dev
 # component =frontend
