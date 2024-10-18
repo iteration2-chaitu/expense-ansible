23d124806831e6da19d5a2b4b4a2fdf071d2e3a6
chaitu <chaitus.ch09@gmail.com>
2024-10-17 21:17:45 -0700

    integrating vault secrets with ansible

--- ansible.sh
+++ ansible.sh
@@ -2,7 +2,7 @@
 component=$1
 
 ansible-playbook get-secrets.yml  -e vault_token=$vault_token
-ansible-playbook -i localhost, -e  expense-pipeline.yml -e role_name=$component -e env=$env -e '@secrets.json'
+ansible-playbook -i localhost,  -e role_name=$component -e env=$env  expense-pipeline.yml -e '@secrets.json'
 #ansible-playbook -i localhost, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=$env
 
 #$component-$env-yml.chaitus.shop ....FOR ANSIBLE
