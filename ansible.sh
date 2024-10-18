d3b8bc9384dcf4b9968662fe2acbe3c5e7e78888
chaitu <chaitus.ch09@gmail.com>
2024-10-17 21:57:39 -0700

    update code to get the app secrets

--- ansible.sh
+++ ansible.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 component=$1
 
-ansible-playbook get-secrets.yml  -e vault_token=$vault_token
-ansible-playbook -i localhost,  -e role_name=$component -e env=$env  expense-pipeline.yml -e '@secrets.json'
+ansible-playbook get-secrets.yml  -e vault_token=$vault_token -e role_name=$component -e env=$env
+ansible-playbook -i localhost,  -e role_name=$component -e env=$env  expense-pipeline.yml -e '@secrets.json' -e '@app.json'
 #ansible-playbook -i localhost, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=$env
 
 #$component-$env-yml.chaitus.shop ....FOR ANSIBLE
