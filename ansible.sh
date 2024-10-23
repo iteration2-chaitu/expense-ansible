782351946206361f9bfd55a09eba6891961cb0b6
chaitu <chaitus.ch09@gmail.com>
2024-10-23 14:34:17 -0700

    try doing this

--- ansible.sh
+++ ansible.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 component=$1
 
-#ansible-playbook get-secrets.yml  -e vault_token=$vault_token -e role_name=$component -e env=$env
-ansible-playbook -i 35.153.134.238, get-secrets.yml  -e vault_token=$vault_token -e role_name=$component -e env=$env
-#ansible-playbook -i localhost,  -e role_name=$component -e env=$env  expense-pipeline.yml -e '@secrets.json' -e '@app.json'
-ansible-playbook -i 35.153.134.238,  -e role_name=$component -e env=$env  expense-pipeline.yml -e '@secrets.json' -e '@app.json'
+ansible-playbook get-secrets.yml  -e vault_token=$vault_token -e role_name=$component -e env=$env
+#ansible-playbook -i 35.153.134.238, get-secrets.yml  -e vault_token=$vault_token -e role_name=$component -e env=$env
+ansible-playbook -i localhost,  -e role_name=$component -e env=$env  expense-pipeline.yml -e '@secrets.json' -e '@app.json'
+#ansible-playbook -i 35.153.134.238,  -e role_name=$component -e env=$env  expense-pipeline.yml -e '@secrets.json' -e '@app.json'
 #rm -rf ~/secrets.json ~/app.json
 #ansible-playbook -i localhost, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=$env
 
