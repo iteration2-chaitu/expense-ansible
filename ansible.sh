9fced24adfdac673ec3ed7c921837c3ffbad00b8
chaitu <chaitus.ch09@gmail.com>
2024-10-23 13:33:53 -0700

    try doing this

--- ansible.sh
+++ ansible.sh
@@ -3,8 +3,8 @@ component=$1
 
 #ansible-playbook get-secrets.yml  -e vault_token=$vault_token -e role_name=$component -e env=$env
 ansible-playbook -i 172.31.35.117, get-secrets.yml  -e vault_token=$vault_token -e role_name=$component -e env=$env
-ansible-playbook -i 172.31.35.117,  -e role_name=$component -e env=$env  expense-pipeline.yml -e '@secrets.json' -e '@app.json'
-rm -rf ~/secrets.json ~/app.json
+#ansible-playbook -i 172.31.35.117,  -e role_name=$component -e env=$env  expense-pipeline.yml -e '@secrets.json' -e '@app.json'
+#rm -rf ~/secrets.json ~/app.json
 #ansible-playbook -i localhost, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense-pipeline.yml -e role_name=$component -e env=$env
 
 #$component-$env-yml.chaitus.shop ....FOR ANSIBLE
